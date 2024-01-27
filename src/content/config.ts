55a12e94f07e8acb996205d50012a20ad2d8b386
Ch3di <chady@example.com>
2024-01-27 22:47:59 +0100

    add first article "Secret - HTB" to the blogs section

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,25 @@
 import { z, defineCollection } from "astro:content";
 
 const blogSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.string().optional(),
+  heroImage: z.string().optional(),
+  badge: z.string().optional()
 });
 
 const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing:  z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
+  title: z.string(),
+  description: z.string(),
+  custom_link_label: z.string(),
+  custom_link: z.string().optional(),
+  updatedDate: z.coerce.date(),
+  pricing: z.string().optional(),
+  oldPricing: z.string().optional(),
+  badge: z.string().optional(),
+  checkoutUrl: z.string().optional(),
+  heroImage: z.string().optional()
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
@@ -29,6 +29,6 @@ const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = {
-    'blog': blogCollection,
-    'store': storeCollection
-}
+  blog: blogCollection,
+  store: storeCollection
+};
